00d4c31b55eae707ee1ed063a8bdb712b3d57604
stfrenky <shoma.albakov69@gmail.com>
2023-05-16 17:05:04 +0300

    UpDate: TBtn.vue

--- src/components/Card/components/TBtn.vue
+++ src/components/Card/components/TBtn.vue
@@ -9,8 +9,8 @@
 
 <style lang="scss">
   .t-btn {
-    width: 54px;
-    height: 27px;
+    min-width: 54px;
+    min-height: 27px;
     background-color: $accent;
     border-radius: 7px;
     text-transform: uppercase;
